a45a633226fa376e661e8d1e24b9faa6a166745b
Victor Shevchenko <13shev.js@gmail.com>
2022-10-15 02:56:50 +0300

    feat: implement count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(/* matrix */ args) {
+  let count = 0;
+  for (let iRow = 0; iRow < args.length; iRow++) {
+    for (let iCol = 0; iCol < args[iRow].length; iCol++) {
+      if (args[iRow][iCol] == '^^') count++;
+    }
+  }
+  return count;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
